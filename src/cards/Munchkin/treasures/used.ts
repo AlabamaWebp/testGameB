e37a4581bd357d64af26b6d02c1750125c9f4f14
AlabamaWebp <dimyrp@yandex.ru>
2024-03-04 08:36:51 +0300

    Функция для записи используемых карт

--- src/cards/Munchkin/treasures/used.ts
+++ src/cards/Munchkin/treasures/used.ts
@@ -1,25 +1,34 @@
-import { TreasureCard} from "src/data/mucnhkin";
+import { TreasureCard } from "src/data/mucnhkin";
+import { TreasureData } from "src/data/munchkin/interfaces";
 
+function createUsed(
+    name: string,
+    desc: string,
+    defs: TreasureData
+) {
+    return new TreasureCard(
+        name,
+        desc,
+        defs
+    )
+}
 const type = "Используемая"
 export const USED: TreasureCard[] = [
-    new TreasureCard(
-        "Закорми мастера",
-        "",
+    createUsed("Закорми мастера", "",
         {
             treasureType: type,
             defs: {
                 action: (defs) => { defs.player.changeLvl(1) }
             }
-        },
+        }
     ),
-    new TreasureCard(
-        "Примени непонятные правила",
-        "",
+    createUsed("Примени непонятные правила", "",
         {
             treasureType: type,
             defs: {
                 action: (defs) => { defs.player.changeLvl(1) }
             }
-        },
+        }
     ),
+
 ]
